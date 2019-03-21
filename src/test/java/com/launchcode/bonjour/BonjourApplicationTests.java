ac84cc5faf98f894b7822b479e5113ad8bf2c1e8
The-Vampiire <25523682+the-vampiire@users.noreply.github.com>
2019-03-21 13:50:06 -0400

    Initial solution

---
+++ src/test/java/com/launchcode/bonjour/BonjourApplicationTests.java
@@ -0,0 +1,16 @@
+package com.launchcode.bonjour;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class BonjourApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
